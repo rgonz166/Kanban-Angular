c9375499352dd46dc3a2439c261853c302a099ba
Raul Gonzalez <rgonz166@gmail.com>
2020-09-07 20:15:19 -0700

    Add: onSubmit form functionatility (loging, signup, password reset)

--- src/app/user/email-login/email-login.component.ts
+++ src/app/user/email-login/email-login.component.ts
@@ -71,7 +71,27 @@ export class EmailLoginComponent implements OnInit {
   }
 
   async onSubmit() {
-    
+    this.loading = true;
+
+    const email = this.email.value;
+    const password = this.password.value;
+
+    try {
+      if (this.isLogin) {
+        await this.afAuth.signInWithEmailAndPassword(email, password);
+      }
+      if (this.isSignup) {
+        await this.afAuth.createUserWithEmailAndPassword(email, password);
+      }
+      if (this.isPasswordReset) {
+        await this.afAuth.sendPasswordResetEmail(email);
+        this.serverMessage = 'Check your email';
+      }
+    } catch ( err ) {
+      this.serverMessage = err;
+    }
+
+    this.loading = false;
 
   }
 
