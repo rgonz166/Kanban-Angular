aeebede6bfc76edac59f4ee408d99bd54a09becb
Raul Gonzalez <rgonz166@gmail.com>
2020-09-07 18:36:17 -0700

    Add: getters for login, signup and reset for easy management

--- src/app/user/email-login/email-login.component.ts
+++ src/app/user/email-login/email-login.component.ts
@@ -10,7 +10,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-email-login',
   templateUrl: './email-login.component.html',
-  styleUrls: ['./email-login.component.scss']
+  styleUrls: ['./email-login.component.scss'],
 })
 export class EmailLoginComponent implements OnInit {
   form: FormGroup;
@@ -20,9 +20,60 @@ export class EmailLoginComponent implements OnInit {
 
   serverMessage: string;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.minLength(6)], Validators.required],
+      passwordConfirm: ['', []]
+
+    })
   }
+
+  changeType(val) {
+    this.type = val;
+  }
+
+  /* Makes logic in HTML manageable */
+  get isLogin() {
+    return this.type === 'login';
+  }
+
+  get isSignup() {
+    return this.type === 'signup';
+  }
+
+  get isPasswordReset() {
+    return this.type === 'reset';
+  }
+
+  /* Form Controls from Reactive Form */
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  get passwordConfirm() {
+    return this.form.get('passwordConfirm');
+  }
+
+  get passwordDoesMatch() {
+    return this.type !== 'signup' ? true : this.password.value === this.passwordConfirm.value;
+    // if (this.type !== 'signup') {
+    //   return true;
+    // } else {
+    //   return this.password.value === this.passwordConfirm.value;
+    // }
+  }
+
+  async onSubmit() {
+    
+
+  }
+
 
 }
