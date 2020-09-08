a02deb572839f6b775f319fa26da7819e599c6a8
Raul Gonzalez <rgonz166@gmail.com>
2020-09-08 03:50:17 -0700

    Add: taskDrop event to board component
    Add: Input decorator for board in board component

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BoardService } from '../board.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  @Input() board;
 
-  constructor() { }
+  constructor(private boardService: BoardService) {}
 
-  ngOnInit(): void {
+  taskDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this.boardService.updateBoard(this.board.id, this.board.tasks);
   }
 
+  ngOnInit(): void {}
 }
