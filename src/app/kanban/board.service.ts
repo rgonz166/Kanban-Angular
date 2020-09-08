02b00aa6885d2b6e3d0cd3640d2108a6616ce2ce
Raul Gonzalez <rgonz166@gmail.com>
2020-09-07 23:00:05 -0700

    Add: Board Service
    Add: create board function
    
    Add: Delete board function

---
+++ src/app/kanban/board.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import { switchMap } from 'rxjs/operators';
+import { Board, Task } from './board.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+
+  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) { }
+
+
+  /**
+   * Creates new board for the current user
+   */
+  async creatBoard(data: Board) {
+    const user = await this.afAuth.currentUser;
+    return this.db.collection('boards').add({
+      ...data,
+      uid: user.uid,
+      tasks: [{ description: 'Hello!', label: 'yellow' }]
+    });
+  }
+
+  /**
+  * Delete board
+  */
+  deleteBoard (boardId: string) {
+    return this.db
+      .collection('boards')
+      .doc(boardId)
+      .delete();
+  }
+
+}
